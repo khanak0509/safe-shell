e161581adce7aaa61861f91d5834559deeabd3b7
khanak0509 <kkhanak512@gmail.com>
2025-12-28 13:40:42 +0530

    add functions

--- main.py
+++ main.py
@@ -4,4 +4,39 @@ from dotenv import load_dotenv
 import json 
 import os 
 import time
+from Schema_class import * 
+
+def NormalizeCommandNode(state : State):
+    pass
+
+
+def CollectContextNode(state : State):
+    pass
+
+
+def RuleBasedRiskNode(state : State):
+    pass
+
+
+def ContextRiskAdjustmentNode(state : State):
+    pass
+
+
+def LLMExplanationNode(state : State):
+    pass
+
+
+def DecisionNode(state : State):
+    pass
+
+
+def NormalizeCommandNode(state : State):
+    pass
+
+
+def CollectContextNode(state : State):
+    pass
+
+
+
 
