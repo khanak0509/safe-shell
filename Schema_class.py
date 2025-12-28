90c66e65bf9903e05ffb5bb0ff0926ef0b9195c8
khanak0509 <kkhanak512@gmail.com>
2025-12-28 14:37:18 +0530

    change in schema class

--- Schema_class.py
+++ Schema_class.py
@@ -3,18 +3,18 @@ from pydantic import BaseModel
 
 class State(BaseModel):
     raw_command: str
-    normalized_command: str
-    commands: list[str]      
-    cwd: str
-    is_root_dir: bool
-    is_root_user: bool
-    os_type: str
-    rule_risk: Literal["NONE" , "LOW" , "MEDIUM" , "HIGH" , "CRITICAL"]
-    context_risk: Literal["NONE" , "LOW" , "MEDIUM" , "HIGH" , "CRITICAL"]
-    final_risk: str
-    decision: str
-    explanation: str
-    precautions: str
-    safer_alternative: str
+    normalized_command: str = ""
+    commands: list[str] = []
+    cwd: str = ""
+    is_root_dir: bool = False
+    is_root_user: bool = False
+    os_type: str = ""
+    rule_risk: Literal["NONE" , "LOW" , "MEDIUM" , "HIGH" , "CRITICAL"] = "NONE"
+    context_risk: Literal["NONE" , "LOW" , "MEDIUM" , "HIGH" , "CRITICAL"] = "NONE"
+    final_risk: str = "NONE"
+    decision: str = ""
+    explanation: str = ""
+    precautions: str = ""
+    safer_alternative: str = ""
 
 
